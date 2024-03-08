4971d6167b1469db93d9dd986d26a0d61ae9121e
IbAHas <asd191928@gmail.com>
2024-03-08 15:07:34 +0200

    Card-ListView-ListViewBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ void main() => runApp(const MyApp());
 
 Color w = Colors.white;
 Color b = Colors.black;
+Color g = Colors.greenAccent;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -45,6 +46,10 @@ class _MyHomePage extends State<MyHomePage> {
     Info(name: 'Ibrahim3', height: 180, date: DateTime.now()),
     Info(name: 'Ibrahim4', height: 190, date: DateTime.now()),
     Info(name: 'Ibrahim5', height: 195, date: DateTime.now()),
+    Info(name: 'Ibrahim6', height: 195, date: DateTime.now()),
+    Info(name: 'Ibrahim7', height: 195, date: DateTime.now()),
+    Info(name: 'Ibrahim8', height: 195, date: DateTime.now()),
+    Info(name: 'Ibrahim9', height: 195, date: DateTime.now()),
   ];
 
   // final x1 = Info(name: 'Ibrahim1', height: 175, date: DateTime.now());
@@ -82,27 +87,88 @@ class _MyHomePage extends State<MyHomePage> {
         //     ],
         //   ),
         // ),
-        child: Column(
-          children: <Widget>[
-            ...li.map((val) {
-              return Container(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      // crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        MyText(val.name, s20),
-                        MyText(' ${val.height}', s20),
-                      ],
-                    ),
-                    MyText(' ${val.date}', s24)
-                  ],
+        // child: Column(
+        //   children: <Widget>[
+        //     ...li.map((val) {
+        //       return Container(
+        //         padding: const EdgeInsets.all(8),
+        //         child: Column(
+        //           children: <Widget>[
+        //             Row(
+        //               // crossAxisAlignment: CrossAxisAlignment.center,
+        //               mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //               children: <Widget>[
+        //                 MyText(val.name, s20),
+        //                 MyText(' ${val.height}', s20),
+        //               ],
+        //             ),
+        //             MyText(' ${val.date}', s24)
+        //           ],
+        //         ),
+        //       );
+        //     })
+        //   ],
+        // ),
+        //ListView.builder, ListView Or add a parent SingleChildScrollView
+        child: ListView.builder(
+          itemCount: li.length, //Or count if item need to show it.
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(4),
+              child: Card(
+                color: g,
+                shadowColor: w,
+                elevation: 10,
+                //Or container
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          MyText(li[index].name, s20),
+                          MyText(' ${li[index].height}', s20),
+                        ],
+                      ),
+                      MyText(' ${li[index].date}', s24)
+                    ],
+                  ),
                 ),
-              );
-            })
-          ],
+              ),
+            );
+          },
+          //   children: <Widget>[
+          //     ...li.map((val) {
+          //       //Or container
+          //       return Padding(
+          //         padding: const EdgeInsets.all(4),
+          //         child: Card(
+          //           color: g,
+          //           shadowColor: w,
+          //           elevation: 10,
+          //           //Or container
+          //           child: Padding(
+          //             padding: const EdgeInsets.all(8.0),
+          //             child: Column(
+          //               children: <Widget>[
+          //                 Row(
+          //                   // crossAxisAlignment: CrossAxisAlignment.center,
+          //                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //                   children: <Widget>[
+          //                     MyText(val.name, s20),
+          //                     MyText(' ${val.height}', s20),
+          //                   ],
+          //                 ),
+          //                 MyText(' ${val.date}', s24)
+          //               ],
+          //             ),
+          //           ),
+          //         ),
+          //       );
+          //     })
+          //   ],
         ),
         // child: Column(
         //   children: <Widget>[
