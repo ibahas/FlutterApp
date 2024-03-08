5a513aa9bea1ecce5ab9b494efd7d21814100faf
IbAHas <asd191928@gmail.com>
2024-03-08 12:40:53 +0200

    Stack&Alignmet

--- lib/main.dart
+++ lib/main.dart
@@ -46,33 +46,72 @@ class _MyHomePage extends State<MyHomePage> {
         ),
       ),
       body: Container(
-        color: Colors.redAccent,
+        // color: Colors.redAccent,
+        alignment: Alignment.center,
+        child: Stack(
+          // alignment: AlignmentDirectional.topEnd,
+          // alignment: AlignmentDirectional.topCenter,
+          // alignment: Alignment.topRight,
+          // alignment: Alignment.center,
+          // alignment: Alignment.centerRight,
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              width: 400,
+              height: 400,
+              color: Colors.greenAccent,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      MyText('Aaa', s),
+                      MyText('Bbb', s),
+                      MyText('Ccc', s),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      MyText('Aaa', s),
+                      MyText('Bbb', s),
+                      MyText('Ccc', s),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
         // margin: const EdgeInsets.all(8)
         // margin: const EdgeInsets.fromLTRB(30, 10, 30, 8),
         // margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 18.0),
-        margin: const EdgeInsets.only(top: 10),
-        // padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-        // padding: const EdgeInsets.only(top: 10, bottom: 10),
-        padding: const EdgeInsets.all(8.0),
-        // height: double.infinity,
-        // alignment: Alignment.center,
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: MyText(
-                  'Aaaa',
-                  s,
-                )),
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: MyText(
-                  'Bbbb',
-                  s2,
-                )),
-          ],
-        ),
+        // margin: const EdgeInsets.only(top: 10),
+        // // padding: EdgeInsets.fromLTRB(left, top, right, bottom),
+        // // padding: const EdgeInsets.only(top: 10, bottom: 10),
+        // padding: const EdgeInsets.all(8.0),
+        // // height: double.infinity,
+        // // alignment: Alignment.center,
+        // child: Row(
+        //   // mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     Padding(
+        //         padding: const EdgeInsets.all(8.0),
+        //         child: MyText(
+        //           'Aaaa',
+        //           s,
+        //         )),
+        //     Padding(
+        //         padding: const EdgeInsets.all(8.0),
+        //         child: MyText(
+        //           'Bbbb',
+        //           s2,
+        //         )),
+        //   ],
+        // ),
       ),
     );
   }
