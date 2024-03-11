2fd31a7391dc6b066faaf9794fff534710b5fbd9
IbAHas <asd191928@gmail.com>
2024-03-11 15:03:36 +0200

    Slider done

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //From this
       home: null,
       initialRoute: '/',
       routes: {
-        //Or this
         '/': (context) => const MyHomePage(),
-        // When navigating to the "/" route, build the FirstScreen widget.
         Screen1.routeName: (context) => const Screen1(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
         Screen2.routeName: (context) => const Screen2(),
       },
     );
@@ -35,32 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Map<String, Widget>> _pages = [
-    {
-      'page': const Scaffold(
-        body: Center(
-          child: Text('Text 1'),
-        ),
-      ),
-      'title': const Text('Part 1')
-    },
-    {
-      'page': const Scaffold(
-        body: Center(
-          child: Text('Text 2'),
-        ),
-      ),
-      'title': const Text('Part 2')
-    },
-  ];
-
-  int _selectedPageIndex = 0;
-
-  void _x1(int index) {
-    setState(() {
-      _selectedPageIndex = index;
-    });
-  }
+  late double _value = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -68,30 +39,27 @@ class _MyHomePageState extends State<MyHomePage> {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: _pages[_selectedPageIndex]['title'],
+          title: const Text('Demo'),
         ),
-        body: _pages[_selectedPageIndex]['page'],
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.white,
-          currentIndex: _selectedPageIndex,
-          selectedFontSize: 15,
-          unselectedFontSize: 15,
-          type: BottomNavigationBarType.shifting,
-          onTap: _x1,
-          items: const [
-            BottomNavigationBarItem(
-              backgroundColor: Colors.pinkAccent,
-              icon: Icon(Icons.category),
-              label: 'Part 1',
-              tooltip: 'Part 1',
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              width: _value,
+              height: _value,
             ),
-            BottomNavigationBarItem(
-              backgroundColor: Colors.pinkAccent,
-              icon: Icon(Icons.star),
-              label: 'Part 2',
-              tooltip: 'Part 2',
-            ),
+            Text('Value: ${(_value).round()}'),
+            Slider(
+              value: _value,
+              onChanged: (double val) => setState(() => _value = val),
+              min: 0,
+              max: 500,
+              inactiveColor: Colors.red,
+              activeColor: Colors.greenAccent,
+              divisions: 100,
+              label: 'Value',
+            )
           ],
         ),
         drawerScrimColor: Colors.pinkAccent.withOpacity(0.3),
